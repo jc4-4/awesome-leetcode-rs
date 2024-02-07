84a50dfd3dfe68a2ba7af85970d348f4a90566db
jc4_4 <1950050+jc4-4@users.noreply.github.com>
2024-02-07 12:34:33 +0800

    Add palindrome_number

---
+++ src/problems/palindrome_number.rs
@@ -0,0 +1,45 @@
+use std::collections::VecDeque;
+
+// https://leetcode.com/problems/palindrome-number/
+pub fn is_palindrome(x: i32) -> bool {
+    if x < 0 {
+        return false;
+    }
+
+    // push digits into a deque
+    let mut y = x;
+    let mut deq = VecDeque::with_capacity(10);
+    while y > 0 {
+        deq.push_front(y % 10);
+        y /= 10;
+    }
+
+    // check mismatch in the first and last digits
+    while deq.len() >= 2 {
+        if deq.pop_front().unwrap()!= deq.pop_back().unwrap() {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_case1() {
+        assert_eq!(is_palindrome(121), true);
+    }
+
+    #[test]
+    fn test_case2() {
+        assert_eq!(is_palindrome(-121), false);
+    }
+
+    #[test]
+    fn test_case3() {
+        assert_eq!(is_palindrome(10), false);
+    }
+}
